29bbfe4c79a3d54bf01f3e8c5aabbcf5b8bd7fc8
tiantianjiayou <45349294+jiayoutiantian@users.noreply.github.com>
2018-12-23 17:41:50 +0800

    Update 在O(1)时间内删除链表结点.py
    tiantian's solution

--- Target Offer/在O(1)时间内删除链表结点.py
+++ Target Offer/在O(1)时间内删除链表结点.py
@@ -1,52 +1,48 @@
 '''
 给定单向链表的头指针和一个结点指针,定义一个函数在O(1)时间删除该结点
 '''
+import pdb
 class ListNode:
-    def __init__(self, x=None):
-        self.val = x
-        self.next = None
-    def __del__(self):
-        self.val = None
-        self.next = None
-
-class Solution:
-    def DeleteNode(self, pListHead, pToBeDeleted):
-        if not pListHead or not pToBeDeleted:
-            return None
-
-        if pToBeDeleted.next != None:
-            pNext = pToBeDeleted.next
-            pToBeDeleted.val = pNext.val
-            pToBeDeleted.next = pNext.next
-            pNext.__del__()
-
-
-        elif pListHead == pToBeDeleted:
-            pToBeDeleted.__del__()
-            pListHead.__del__()
-        else:
-            pNode = pListHead
-            while pNode.next != pToBeDeleted:
-                pNode = pNode.next
-            pNode.next = None
-            pToBeDeleted.__del__()
+	def __init__(self,val=None):
+		self.val=val
+		self.next=None
+class solution:
+	def DeleteListNode(self,head,targetVal=None):
+		head_list = head
+		if head==None:
+			return 
+		if head.next==None:
+			head=None
+		while head.val!=targetVal:
+			head=head.next
+			if head==None:
+				print('cannot find the target value %d' % targetVal)
+				return False
+				break
+		ptr = head
+		head=head.next
+		ptr.val=head.val
+		head.val=targetVal
+		ptr.next=head.next
+		while head_list!=None:
+			print(head_list.val)
+			head_list=head_list.next
+		return True
 
 
-node1 = ListNode(10)
-node2 = ListNode(11)
-node3 = ListNode(13)
-node4 = ListNode(15)
-node1.next = node2
-node2.next = node3
-node3.next = node4
+		return False
 
-S = Solution()
-S.DeleteNode(node1, node3)
-print(node3.val)
-S.DeleteNode(node1, node3)
-print(node3.val)
-print(node2.val)
-S.DeleteNode(node1, node1)
-print(node1.val)
-S.DeleteNode(node1, node1)
-print(node1.val)
+s=solution()
+node1=ListNode(1)
+node2=ListNode(2)
+node3=ListNode(3)
+node4=ListNode(4)
+node5=ListNode(5)
+node1.next=node2
+node2.next=node3
+node3.next=node4
+node4.next=node5
+flag=s.DeleteListNode(node1,7)
+print(flag)
+flag=s.DeleteListNode(None)
+print(flag)
